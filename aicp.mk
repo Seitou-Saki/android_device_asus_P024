f6797d9b022ad2d961f3764c2f81413b9f44b6af
Seitou Saki <seitou.shrine@gmail.com>
2017-04-14 22:03:45 +0900

    AICP-ify

--- aicp.mk
+++ aicp.mk
@@ -1,11 +1,13 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/configs/common.mk)
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/asus/P024/device.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 800
 TARGET_SCREEN_HEIGHT := 1280
+-include vendor/aicp/configs/bootanimation.mk
 
 
 # Release name. Device identifier. This must come after all inclusions
@@ -13,6 +15,8 @@ PRODUCT_DEVICE := P024
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := P024
 PRODUCT_MANUFACTURER := asus
-PRODUCT_NAME := lineage_P024
+PRODUCT_NAME := aicp_P024
 
-
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Seitou Saki(Unofficial)"
